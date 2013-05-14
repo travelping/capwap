616a67a5f15e671d80430b1bbd89e20fea9eb15d
Andreas Schultz <aschultz@tpip.net>
2013-05-14 15:02:30 +0200

    remove tetrapak run target, not needed

--- tetrapak/capwap_run.erl
+++
@@ -1,8 +0,0 @@
--module(capwap_run).
-
--task({"run:capwap", "Start CAPWAP"}).
-
-run("run:capwap", _) ->
-    tetrapak:require("build:erlang"),
-    [ok = application:start(App) || App <- [sasl, regine, crypto, public_key, ssl, capwap]],
-    ok.
