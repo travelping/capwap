4bac0234294e59d8ea2d22b0b4aeeb770ea5b9b7
Tino Breddin <tb@travelping.com>
2013-08-11 19:06:37 +0200

    Don't close socket

--- src/capwap_ac.erl
+++ src/capwap_ac.erl
@@ -572,7 +572,7 @@ terminate(Reason, StateName, #state{peer_data = PeerId, event_log=EventLog, sess
     end,
     %ctld_session:stop(Session, []),
     ctld_session:terminate(Session),
-    socket_close(Socket),
+    %socket_close(Socket),
     stop_trace(EventLog),
     ok.
 
