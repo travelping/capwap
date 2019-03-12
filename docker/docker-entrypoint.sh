cd305538ca1cb0f0b784f1b441111d7f4e4fbbb2
Andreas Schultz <andreas.schultz@travelping.com>
2019-03-12 14:17:32 +0100

    include ergw-capwap-node docker build in main repo

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+!/bin/sh
+# copy of configuration files
+RELPATH=$(find /opt/ergw-capwap-node/releases/ -mindepth 1 -maxdepth 1 -type d)
+[ -f /config/ergw-capwap-node/sys.config ] && cp /config/ergw-capwap-node/sys.config $RELPATH/sys.config
+[ -f /config/ergw-capwap-node/vm.args ] && cp /config/ergw-capwap-node/vm.args $RELPATH/vm.args
+
+exec "$@"
